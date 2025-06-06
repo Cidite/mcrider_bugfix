242b5ab48ce1ec29e2babaaef74aa6eace7fbd53
BuildTools <unconfigured@null.spigotmc.org>
2025-06-06 23:54:02 +0900

    미사용 코드 제거

--- src/client/java/com/gihub/cidite/test/mcrider_bug_fix/client/Mcrider_bug_fixClient.java
+++ src/client/java/com/gihub/cidite/test/mcrider_bug_fix/client/Mcrider_bug_fixClient.java
@@ -1,29 +1,16 @@
 package com.gihub.cidite.test.mcrider_bug_fix.client;
 
 
-import com.gihub.cidite.test.mcrider_bug_fix.client.config.SimpleConfig;
 import net.fabricmc.api.ClientModInitializer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class Mcrider_bug_fixClient implements ClientModInitializer {
     public static final String MOD_ID = "mcrider_fix";
-    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-    public static final int tick_count = 0;
+    //public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
     public static boolean Riding = false;
-    public static boolean isRiding = false;
-    public static byte displayEntityModification = 0;
-    public static SimpleConfig CONFIG = SimpleConfig.of("mcrider_bugfix").provider(Mcrider_bug_fixClient::provider).request();
-    //public static final int gameAcceleration = CONFIG.getOrDefault( "game_acceleration", 3 );
-    public static final int gameAcceleration = 1;
 
 
     @Override
     public void onInitializeClient() {
-    }
-
-    private static String provider(String filename) {
-        return "#게임 배속. 1~6의 숫자만 허용됩니다. \ngame_acceleration=3";
     }
 
 }
--- src/client/java/com/gihub/cidite/test/mcrider_bug_fix/client/mixin/MinecraftClientMixin.java
+++ src/client/java/com/gihub/cidite/test/mcrider_bug_fix/client/mixin/MinecraftClientMixin.java
@@ -4,16 +4,12 @@ package com.gihub.cidite.test.mcrider_bug_fix.client.mixin;
 import com.gihub.cidite.test.mcrider_bug_fix.client.Mcrider_bug_fixClient;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerEntity;
-import net.minecraft.client.render.RenderTickCounter;
 import net.minecraft.client.world.ClientWorld;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.util.profiler.Profiler;
 import net.minecraft.util.profiler.Profilers;
-import net.minecraft.world.tick.TickManager;
 import org.jetbrains.annotations.Nullable;
-import org.slf4j.Logger;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
@@ -21,27 +17,11 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.util.Objects;
-
 
 @Mixin(MinecraftClient.class)
 public abstract class MinecraftClientMixin {
-    @Shadow private final RenderTickCounter.Dynamic renderTickCounter = new RenderTickCounter.Dynamic(((float)Mcrider_bug_fixClient.gameAcceleration * 20), 0L, this::getTargetMillisPerTick);
     @Shadow @Nullable public ClientWorld world;
     @Shadow @Nullable public ClientPlayerEntity player;
-
-    @Shadow @Final private static Logger LOGGER;
-
-    @Unique
-    private float getTargetMillisPerTick(float millis) {
-        if (this.world != null) {
-            TickManager tickManager = this.world.getTickManager();
-            if (tickManager.shouldTick()) {
-                return Math.max(millis, tickManager.getMillisPerTick());
-            }
-        }
-        return millis;
-    }
 
     @Unique
     private boolean isKartEntity() {
@@ -61,25 +41,7 @@ public abstract class MinecraftClientMixin {
         Profiler profiler = Profilers.get();
         profiler.push("isKartEntity");
         if (this.world != null) {
-            if (isKartEntity()) {
-                Mcrider_bug_fixClient.Riding = true;
-                if (Mcrider_bug_fixClient.displayEntityModification == 1) {
-                    Mcrider_bug_fixClient.displayEntityModification = 2;
-                }
-                if (!Mcrider_bug_fixClient.isRiding) {
-                    Mcrider_bug_fixClient.isRiding = true;
-                    Mcrider_bug_fixClient.displayEntityModification = 1;
-                }
-            } else if (Mcrider_bug_fixClient.Riding) {
-                Mcrider_bug_fixClient.Riding = false;
-                Mcrider_bug_fixClient.isRiding = false;
-                if (Mcrider_bug_fixClient.displayEntityModification == -1) {
-                    Mcrider_bug_fixClient.displayEntityModification = 0;
-                }
-                if (Mcrider_bug_fixClient.displayEntityModification == 2) {
-                    Mcrider_bug_fixClient.displayEntityModification = -1;
-                }
-            }
+            Mcrider_bug_fixClient.Riding = isKartEntity();
         }
         profiler.pop();
     }
